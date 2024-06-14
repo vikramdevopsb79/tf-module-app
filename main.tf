97d4bf59cf6ae6dd8e65c4fa02537896811918f9
sai <kuppalasaivikram@gmail.com>
2024-06-14 17:44:47 +0530

    adding label with jinja template

--- main.tf
+++ main.tf
@@ -46,7 +46,6 @@ resource "aws_instance" "node" {
     Monitor = "yes"
     env       = var.env
     component = var.name
-    Monitor = "yes"
   }
 
   # this to not re-create machines on tf-apply again and again. This will not be needed later when we go with ASG
